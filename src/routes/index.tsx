fde8aa8739ae5043eafd7f3515dd652b62bcfe35
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2023-12-16 09:50:46 +0100

    added route back

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ const Routing = () => {
 		<BrowserRouter>
 			<Routes>
 				{/* ONBOARDING ROUTES */}
-				{/* <Route path="/*" element={<Navigate to="/" />} /> */}
+				<Route path="/*" element={<Navigate to="/" />} />
 				<Route path="/" element={<Navigate to="/login" />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
